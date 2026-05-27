6eceba75f5683985dc38e59273b8059496c02996
Allan <allanhu100@gmail.com>
2026-05-26 20:42:06 -0500

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.insertAdjacentHTML("afterbegin", `
+<footer class="footer">
+    &copy; Allan Hu
+    <a href="https://allanhu100.github.io/dev">Developer Portal</a>
+    <a href="https://allanhu100.github.io/credits">Credits</a>
+</footer>
+    `);
+});
