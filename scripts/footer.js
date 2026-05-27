81050e47b146fa170a92e40e7670b173e1fb83f2
Allan <allanhu100@gmail.com>
2026-05-26 21:07:25 -0500

    Delete scripts directory

--- scripts/footer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.body.insertAdjacentHTML("beforeend", `
-<footer class="footer">
-    &copy; Allan Hu
-    <a href="https://allanhu100.github.io/dev">Developer Portal</a>
-    <a href="https://allanhu100.github.io/credits">Credits</a>
-</footer>
-    `);
-});
